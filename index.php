dd8db84c203e771586d1e5e083989dc49999d43b
Daniels <134560764+Daniel-Sequeira@users.noreply.github.com>
2025-08-03 17:26:15 -0600

    update

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-require 'system/session.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <a href="login.php" rel="noopener noreferrer">IR A LOGIN</a>
+</body>
+</html>
